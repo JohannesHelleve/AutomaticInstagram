fe51f45783c3639acdd774cc7a2e006cbd824eb0
Johannes Helleve <Johanshelleve@gmail.com>
2023-05-29 22:45:34 +0200

    Intialize

---
+++ import instaloader.py
@@ -0,0 +1,15 @@
+import instaloader
+
+# Get the most liked photo in Oslo in the last day
+client = instaloader.Instaloader()
+client.login("HelleveMediaGroup@gmail.com", "Kjørdakjør")
+
+# Set the rate controller to sleep for 1 second between requests
+client.set_rate_controller(instaloader.RateController(sleep_time=1))
+
+# Get the most liked photo in Oslo in the last day
+photos = client.get_geotagged_media("59.912898,10.752222", max_likes=1)
+
+# Download the photo
+photo = photos[0]
+client.download_photo(photo, "most_liked_photo_in_oslo.jpg")
